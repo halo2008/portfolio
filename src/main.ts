ab662690249321f758c65041965267f59649d32b
halo2008 <ksedkowski@gmail.com>
2026-03-04 21:55:07 +0100

    feat: Add Google Identity Toolkit, Secure Token, and Firestore API domains to `connectSrc` CSP.

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,7 @@ async function bootstrap() {
                 fontSrc: ["'self'", "https://fonts.gstatic.com"],
                 imgSrc: ["'self'", "data:", "https://grainy-gradients.vercel.app"],
                 frameSrc: ["'self'", "https://www.google.com", "https://www.recaptcha.net", "https://ksedkowski.grafana.net"],
-                connectSrc: ["'self'", "ws:", "wss:", "https://www.google.com", "https://www.gstatic.com"],
+                connectSrc: ["'self'", "ws:", "wss:", "https://www.google.com", "https://www.gstatic.com", "https://identitytoolkit.googleapis.com", "https://securetoken.googleapis.com", "https://firestore.googleapis.com"],
             },
         },
     }));
